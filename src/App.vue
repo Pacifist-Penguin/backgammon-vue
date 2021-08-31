3a712f336b7da2c2ca4a1dd7d945f14842aae6d4
Pacifist-Penguin <7593121+Pacifist-Penguin@users.noreply.github.com>
2021-08-31 03:37:38 +0300

    real init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+	<h1>
+		Hi! It's game called "backgammon", widely popular on east. It's "short" version because i didn't like "long"
+		version.
+	</h1>
+	<button v-show="turnOf === null" @click="firstGameRoll">And the first turn will belong to:</button>
+	{{ turnOf }}
+	<div>
+		<span v-for="(position, index) in firstRow" v-bind:key="index">{{ position }}</span>
+	</div>
+	<div>
+		<span v-for="(position, index) in secondRow" v-bind:key="index">{{ position }}</span>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+	name: "App",
+	data() {
+		return {
+			//contains position of all draughts
+			//upper-right corner -> upper left -> bottom left -> bottom right
+			//positive numbers are light draughts, negative numbers is dark
+			desk: [2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2],
+			turnOf: null, //true === light, false == darks
+			minRoll: 1,
+			maxRoll: 6,
+			turnsLeft: 0,
+			firstRoll: 0,
+			secondRoll: 0,
+			selected: false,
+			indexOfSelectedDraught: 0,
+		};
+	},
+	computed: {
+		firstRow() {
+			return this.desk.slice(0, 12).reverse();
+		},
+		secondRow() {
+			return this.desk.slice(12, 24);
+		},
+		rollsAreEqual() {
+			return this.firstRoll === this.secondRoll;
+		},
+		canGetIn() {
+			return "";
+		},
+		ifThereIsAvailablePositions() {
+			return this.turnOf === true
+				? this.desk[this.indexOfSelectedDraught + this.firstRoll] >= -1 ||
+						this.desk[this.indexOfSelectedDraught + this.secondRoll] >= -1
+				: this.desk[this.indexOfSelectedDraught - this.firstRoll] <= 1 ||
+						this.desk[this.indexOfSelectedDraught - this.secondRoll] <= 1;
+		},
+	},
+	methods: {
+		roll() {
+			return Math.floor(Math.random() * (this.maxRoll - this.minRoll) + 1);
+		},
+		begginingOfTheTurn() {
+			this.firstRoll = this.roll();
+			this.secondRoll = this.roll();
+			if (this.ifThereIsAvailablePositions) {
+				if (this.rollsAreEqual) {
+					this.turnsLeft = 4;
+				} else {
+					this.turnsLeft = 2;
+				}
+			} else {
+				this.theEndOfTurn();
+			}
+		},
+		firstGameRoll() {
+			this.firstRoll = this.roll();
+			this.secondRoll = this.roll();
+			if (this.firstRoll > this.secondRoll) {
+				this.turnOf = true; //it's turn of lights
+			} else if (this.firstRoll < this.secondRoll) {
+				this.turnOf = false; //it's turn of darks
+			} else {
+				this.askForReroll(); //TODO
+			}
+		},
+		select(target) {
+			this.indexOfSelectedDraught = target.value;
+			this.selected = true;
+		},
+		moveDraught() {
+			if (this.ifColumnAvailable) {
+				if (this.turnOf === true) {
+					this.desk[this.indexOfSelectedDraught]--;
+				} else {
+					this.desk[this.indexOfSelectedDraught]++;
+				}
+			}
+		},
+		theEndOfTurn() {
+			this.selected = false;
+			this.turnsLeft = 0;
+			this.turnOf = !this.turnOf;
+		},
+	},
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
