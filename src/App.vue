b8b7b66845c377e0e046a7901f0d156304aa85c5
Pacifist-Penguin <7593121+Pacifist-Penguin@users.noreply.github.com>
2021-09-01 03:18:56 +0300

    something like symbolic mock-up is here

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,26 @@
 	</h1>
 	<button v-show="turnOf === null" @click="firstGameRoll">And the first turn will belong to:</button>
 	{{ turnOf }}
-	<div>
-		<span v-for="(position, index) in firstRow" v-bind:key="index">{{ position }}</span>
+	<h3 v-show="askedForRerroll && turnOf === null">Wow, rolls are equal, please try rerrolling</h3>
+	<div style="display: flex">
+		<span v-for="(position, index) in firstRow" v-bind:key="index">
+			<draught-figure :position="position" :color="'black'" :key="figures" />
+		</span>
 	</div>
-	<div>
-		<span v-for="(position, index) in secondRow" v-bind:key="index">{{ position }}</span>
+	<div style="display: flex">
+		<span v-for="(position, index) in secondRow" v-bind:key="index">
+			<draught-figure :position="position" :color="'black'" :key="figures" />
+		</span>
 	</div>
 </template>
 
 <script>
+import DraughtFigure from "@/components/DraughtFigure.vue";
 export default {
 	name: "App",
+	components: {
+		DraughtFigure,
+	},
 	data() {
 		return {
 			//contains position of all draughts
@@ -25,16 +34,18 @@ export default {
 			turnOf: null, //true === light, false == darks
 			minRoll: 1,
 			maxRoll: 6,
+			askedForRerroll: false,
 			turnsLeft: 0,
 			firstRoll: 0,
 			secondRoll: 0,
 			selected: false,
 			indexOfSelectedDraught: 0,
+			indexOfSelectedColumn: 0,
 		};
 	},
 	computed: {
 		firstRow() {
-			return this.desk.slice(0, 12).reverse();
+			return this.desk.slice(0, 12).reverse(); //it's reversed only for it to display properly
 		},
 		secondRow() {
 			return this.desk.slice(12, 24);
@@ -42,8 +53,19 @@ export default {
 		rollsAreEqual() {
 			return this.firstRoll === this.secondRoll;
 		},
+		darkHome() {
+			return this.desk.slice(0, 6);
+		},
+		lightHome() {
+			return this.desk.slice(18, 24);
+		},
+		possibleToGetIn() {
+			return this.turnOf === true ? this.darkHome.some((el) => el >= -1) : this.lightHome.some((el) => el <= 1);
+		},
 		canGetIn() {
-			return "";
+			return this.turnOf === true
+				? this.darkHome.some((el) => el === this.fristRoll || this.secondRoll)
+				: this.lightHome.some((el) => el === this.firstRoll || this.secondRoll);
 		},
 		ifThereIsAvailablePositions() {
 			return this.turnOf === true
@@ -74,11 +96,11 @@ export default {
 			this.firstRoll = this.roll();
 			this.secondRoll = this.roll();
 			if (this.firstRoll > this.secondRoll) {
-				this.turnOf = true; //it's turn of lights
+				this.turnOf = true; //first turn is the turn of lights
 			} else if (this.firstRoll < this.secondRoll) {
-				this.turnOf = false; //it's turn of darks
+				this.turnOf = false; //first turn is the turn of darks
 			} else {
-				this.askForReroll(); //TODO
+				this.askedForRerroll = true;
 			}
 		},
 		select(target) {
@@ -89,9 +111,12 @@ export default {
 			if (this.ifColumnAvailable) {
 				if (this.turnOf === true) {
 					this.desk[this.indexOfSelectedDraught]--;
+					this.desk[this.indexOfSelectedColumn]++;
 				} else {
 					this.desk[this.indexOfSelectedDraught]++;
+					this.desk[this.indexOfSelectedColumn]--;
 				}
+				this.turnsLeft--;
 			}
 		},
 		theEndOfTurn() {
@@ -102,5 +127,3 @@ export default {
 	},
 };
 </script>
-
-<style></style>
---
+++ src/components/DraughtFigure.vue
@@ -0,0 +1,35 @@
+<template>
+	<ul style="display: block; height: 50px; list-style: none">
+		<li v-for="(draughts, index) in column" :key="index">
+			<svg style="width: 10px" viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
+				<circle cx="60" cy="60" r="50" :fill="draughtColor" />
+			</svg>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	name: "draught",
+	props: {
+		color: {
+			type: String,
+			required: true,
+			default: "white",
+		},
+		position: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+	},
+	computed: {
+		draughtColor() {
+			return this.position > 0 ? "red" : "black";
+		},
+		column() {
+			return Math.abs(this.position);
+		},
+	},
+};
+</script>
