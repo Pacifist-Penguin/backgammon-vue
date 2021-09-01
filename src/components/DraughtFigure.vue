0eb501828032fd1800930f7a19bcb1c4a7222034
Pacifist-Penguin <7593121+Pacifist-Penguin@users.noreply.github.com>
2021-09-01 19:24:02 +0300

    created necessary computed properties and hopefully i'm half way to get done

--- src/components/DraughtFigure.vue
+++ src/components/DraughtFigure.vue
@@ -1,17 +1,21 @@
 <template>
-	<ul style="display: block; height: 50px; list-style: none">
-		<li v-for="(draughts, index) in column" :key="index" @click="selectDraught">
-			<svg style="width: 10px" viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
-				<circle cx="60" cy="60" r="50" :fill="draughtColor" />
-			</svg>
-			{{ indexOfColumnOnDesk }}
-		</li>
-	</ul>
+	<svg
+		@click.stop="selectDraught"
+		style="width: 10px"
+		viewBox="0 0 120 120"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<circle cx="60" cy="60" r="50" :fill="draughtColor" />
+	</svg>
 </template>
 
 <script>
 export default {
 	name: "draught",
+	emits: {
+		selected: (value) => typeof value === "number",
+	},
 	props: {
 		draughtNumber: {
 			type: Number,
@@ -27,14 +31,11 @@ export default {
 		draughtColor() {
 			return this.draughtNumber > 0 ? "red" : "black";
 		},
-		column() {
-			return Math.abs(this.draughtNumber);
-		},
 	},
 	methods: {
 		selectDraught() {
 			this.$emit("selected", this.indexOfColumnOnDesk);
-			console.log("selected", this.indexOfColumnOnDesk);
+			console.log("selected figure number:", this.indexOfColumnOnDesk);
 		},
 	},
 };
