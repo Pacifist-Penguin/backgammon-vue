2df0291a721030d4fdc083c009343b16422ba1dc
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2021-09-24 17:48:20 +0300

    Score works properly & modals refactored

--- src/App.vue
+++ src/App.vue
@@ -10,46 +10,76 @@
 		</p>
 	</header>
 	<section>
-		<game-desk @won="scoreOnEmit" :firstTurnOf="firstTurnOfIn" />
+		<game-desk v-if="firstTurnOfIn != null" @won="scoreOnEmit" :firstTurnOf="firstTurnOfIn" />
 	</section>
 	<modal-pick v-if="modalVisible" @firstTurnOf="begginingOfTheGame" />
+	<modal-post-game
+		v-if="postGameModalVisible"
+		:lightsScore="score.true"
+		:darksScore="score.false"
+		@hide-me="hidePostGameModal"
+	/>
+	<modal-match-end v-if="gameEnded" :winner="winner" />
 </template>
 
 <script>
 import GameDesk from "@/components/GameDesk.vue";
 import ModalPick from "@/components/ModalPick.vue";
+import ModalPostGame from "@/components/ModalPostGame.vue";
+import ModalMatchEnd from "@/components/ModalMatchEnd.vue";
 
 export default {
 	name: "App",
 	components: {
 		GameDesk,
-		ModalPick
+		ModalPick,
+		ModalPostGame,
+		ModalMatchEnd
 	},
 	data() {
 		return {
 			score: {
-				true: 0, //lights, it's name as bool because i can save lil memory like this
+				true: 0, //lights, it's named as boolean because i can save lil memory like this
 				false: 0 //darks
 			},
 			firstTurnOfIn: null,
-			modalVisible: true
+			modalVisible: true,
+			postGameModalVisible: false,
+			gameEnded: false,
+			winner: undefined
 		};
 	},
 	methods: {
 		scoreOnEmit(value) {
 			this.score[value.winner] += value.score;
+			if (this.score[value.winner] >= 5) {
+				this.gameEnds();
+			} else {
+				this.postGameModalVisible = true;
+			}
+		},
+		hidePostGameModal() {
+			this.postGameModalVisible = false;
 		},
 		begginingOfTheGame(value) {
 			this.firstTurnOfIn = value;
 			this.modalVisible = false;
+		},
+		gameEnds() {
+			this.gameEnded = true;
 		}
 	},
 	watch: {
 		score: function () {
 			if (this.score.true >= 5) {
 				console.log("lights won!");
+				this.winner = "lights";
+				this.gameEnds();
 			} else if (this.score.false >= 5) {
-				console.log("darks won!");
+				this.winner = "darks";
+				this.gameEnds();
+			} else {
+				this.postGameModalVisible = true;
 			}
 		}
 	}
@@ -62,8 +92,12 @@ html {
 	background-repeat: no-repeat;
 	background-size: cover;
 	background-position: center;
+	font-family: sans-serif;
 }
 body {
 	background-color: rgba(255, 255, 255, 0.6);
+	width: 100vw;
+	height: 100vh;
+	margin: 0px;
 }
 </style>
---
+++ src/components/ModalMatchEnd.vue
@@ -0,0 +1,21 @@
+<template>
+	<modal-outer>
+		<h1>Congtrats, {{ winner }} won!</h1>
+	</modal-outer>
+</template>
+
+<script>
+import ModalOuter from "@/components/ModalOuter.vue";
+export default {
+	name: "ModalGameEnd",
+	components: {
+		ModalOuter
+	},
+	props: {
+		winner: {
+			type: Boolean,
+			required: true
+		}
+	}
+};
+</script>
